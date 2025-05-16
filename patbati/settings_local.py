41d8fa691e0bdea4f6bc82b78b9f8a6c9e5072b1
TheoLechemia <lechemia.theo@gmail.com>
2025-05-16 09:53:11 +0200

    starter architecture

---
+++ patbati/settings_local.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': "patbati",
+        'USER':"geonatadmin",
+        'PASSWORD': "monpassachanger",
+        'HOST': "localhost",
+        'PORT': 5432,
+
+    }
+}
