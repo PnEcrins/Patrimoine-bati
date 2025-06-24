26a35a37809e9b8acfc98a558c15472b663f8ca2
glebiskava <leopold.mittelberger@gmail.com>
2025-06-24 14:11:40 +0200

    - add admin for nomenclature

--- patbati/bati/admin.py
+++ patbati/bati/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
-
-from patbati.bati.models import Bati
+from patbati.bati.models import Bati, Nomenclature, NomenclatureType
 from leaflet.admin import LeafletGeoAdmin
 
-# Register your models here.
+@admin.register(Bati)
+class BatiAdmin(LeafletGeoAdmin):
+    list_display = (
+        "id",
+        "valide",
+        "appelation",
+        "secteur",
+        "conservation",
+        "notepatri",
+        "classe",
+        "date_insert",
+        "date_update",
+    )
+    search_fields = ("appelation", "proprietaire", "cadastre", "lieu_dit")
+    list_filter = ("secteur", "conservation", "classe", "valide", "bat_suppr")
 
-admin.site.register(Bati, LeafletGeoAdmin)
+@admin.register(Nomenclature)
+class NomenclatureAdmin(admin.ModelAdmin):
+    list_display = (
+        "id_nomenclature",
+        "id_type",
+        "label",
+        "description",
+        "parentId",
+    )
+    search_fields = ("label", "description")
+    list_filter = ("id_type",)
+
+@admin.register(NomenclatureType)
+class NomenclatureTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "id_type",
+        "label",
+        "definition",
+        "code",
+    )
+    search_fields = ("label", "code")
